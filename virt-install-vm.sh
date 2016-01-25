4ec84acae6858585b82aefd60f2431796a0a585b
Archit Sharma <arcsharm@redhat.com>
2016-01-25 18:04:04 +0530

    add changes to virt-install script to include .ks file

--- virt-install-vm.sh
+++ virt-install-vm.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+
 #Prepare kickstartfile to $dist-vm.ks and start install
+
 if [ -z $5 ];
         then
                 echo "Syntax:  ./virt-install-vm.sh disk_format size vcpu mem dist"
                 echo "Syntax:  ./virt-install-vm.sh <qcow2/raw> 10G 2 1024 fedora22"
         exit 1
 fi
+
 vm=master
 bridge=virbr0
 disk_format=$1
@@ -13,11 +16,14 @@ size=$2
 vcpu=$3
 mem=$4
 dist=$5
+# please note: this repo only contains fedora22's ks_file,
+# please be careful about the .ks URL when you choose a different distro.
+ks_file="https://raw.githubusercontent.com/psuriset/kvm_io/master/$dist-vm.ks"
 
 master_image=master.$disk_format
 image_path=/var/lib/libvirt/images
 yum install -y wget
-wget $ks_file
+wget $ks_file -O /src/$dist-vm.ks
 extra="ks=file:/$disk-vm.ks console=ttyS0,115200"
 if ! rpm -qa | grep -qw virt-install; then
     yum install -y virt-install 
@@ -34,12 +40,14 @@ for i in `seq 1 $nr_vms`; do
 	/bin/rm -f $image_path/vm*.qcow2
 	set +x
 done
+
 echo creating new master image
 qemu-img create -f $disk_format $image_path/$master_image $size
 sync
 echo undefining master xml
 virsh list --all | grep master && virsh undefine master
 echo calling virt-install
+
 virt-install --name=$vm\
 	 --virt-type=kvm\
 	 --disk format=qcow2,path=$image_path/$master_image\
@@ -50,7 +58,7 @@ virt-install --name=$vm\
 	 --os-variant=$dist\
 	 --graphics none\
 	 --extra-args="$extra"\
-	 --initrd-inject=$dist-vm.ks\
+	 --initrd-inject=/src/$dist-vm.ks\
 	 --serial pty\
 	 --serial file,path=/tmp/$vm.console\
 	 --location=$location\
